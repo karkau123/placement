bd95904defe5e7b0c1e23d555b67fc4d4c6ba355
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2024-10-05 20:56:07 +0530

    cses_1 solves

---
+++ CSES/Searching - Sorting/apartments.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define lp(i,a,b) for(ll i=a;i<b;i++)
+#define lpa(i,a,b,x) for(ll i=a;i<b;i+=x)
+#define pl(i,a,b) for(ll i=a;i>=b;i--)
+#define pb push_back
+#define ms(a,x) memset(a,x,sizeof(a))
+#define all(v) v.begin(),v.end()
+#define pll pair<ll,ll>
+#define vpll vector<pll>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define ff first
+#define ss second
+using namespace std;
+ int main()
+{
+  ll n , m , k;
+  cin >> n >>m >>k ;
+  vll a (n) , b(m);
+  lp (i , 0 , n ) cin >> a[i];
+  lp (i , 0 ,m) cin >> b[i];
+  return 0;
+}
